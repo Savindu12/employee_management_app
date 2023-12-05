e43c5b79b3a0fbe0b928ca8c13cd549523970702
savindu <savindudhamsara2001@gmail.com>
2023-12-05 14:04:56 +0530

    Tailwind CSS added

--- ems_frontend/src/app/app.module.ts
+++ ems_frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    EmployeeListComponent
   ],
   imports: [
     BrowserModule,
---
+++ ems_frontend/src/app/models/employee.ts
@@ -0,0 +1,7 @@
+export interface Employee {
+    id: string,
+    employeeName: string,
+    email: string,
+    phone: number,
+    salary: number
+}
